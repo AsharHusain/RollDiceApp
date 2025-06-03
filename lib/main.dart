3807fc99108e10e97460192638ea20bcb892f11f
AsharHusain <ashar02020@gmail.com>
2025-06-03 12:38:42 +0530

    made some changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,27 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Scaffold(body: Center(child: Text("Roll Dice app"))),
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.bottomLeft,
+              end: Alignment.topRight,
+              colors: [
+                const Color.fromARGB(255, 30, 149, 247),
+                const Color.fromARGB(255, 1, 70, 126),
+              ],
+            ),
+          ),
+          child: Center(
+            child: Text(
+              "Roll Dice app",
+              style: TextStyle(color: Colors.pink, fontSize: 35),
+            ),
+          ),
+        ),
+      ),
     ),
   );
 }
